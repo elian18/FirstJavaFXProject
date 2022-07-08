ca0d80e93081325109516d5844b1e6cdba24b9f8
SebastianRoblez <sebastian.roblez@epn.edu.ec>
2022-07-07 23:23:58 -0500

    Inicio de sesion

--- src/controller/HomeScreenController.java
+++ src/controller/HomeScreenController.java
@@ -2,12 +2,16 @@ package controller;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class HomeScreenController {
@@ -16,19 +20,51 @@ public class HomeScreenController {
 
     @FXML
     private TextField txtUser;
+    @FXML
+    private PasswordField txtPasword;
+
 
     @FXML
-    void showWindow2(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Window2.fxml"));
-        Parent root = loader.load();
-        MainSceneController controller = loader.getController();
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();      // Crea nueva ventana
-        stage.setScene(scene);
-        controller.init(txtUser.getText(), stage, this);
-        stage.show();
-        this.stage.close();         
+    private void eventKey(KeyEvent event)
+    {
+        Object evt = event.getSource();
+        if(evt.equals(txtUser))
+        {
+            if(event.getCharacter() .equals(" "))
+            {
+                event.consume();
+            }
+        }else if(evt.equals(txtPasword))
+        {
+            if(event.getCharacter().equals(" "))
+            {
+                event.consume();
+            }
+        }
     }
+
+    /**
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void showWindow2(ActionEvent event) throws IOException {
+        if(txtUser.getText().equalsIgnoreCase("admin")  ){
+            if(txtPasword.getText().equalsIgnoreCase("admin")){//comparar inicio de sesion
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Window2.fxml"));
+                Parent root = loader.load();
+                MainSceneController controller = loader.getController();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();      // Crea nueva ventana
+                stage.setScene(scene);
+                controller.init(txtUser.getText(), stage, this);
+                stage.show();
+                this.stage.close();   
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Usuario y/o contraseña incorrecta");
+            }
+        }
 
     public void setStage(Stage primaryStage) {
         stage = primaryStage;   // Guardar ventana para que el controllador no elimine la ventana
